bf664dfe9f4a39dd9714620760447017e2fe5cf1
Josh Bright <josh.bright@gmail.com>
2024-01-25 10:51:56 -0800

    Added golf course detail page

--- app/home/urls.py
+++ app/home/urls.py
@@ -7,4 +7,5 @@ app_name = "home"
 urlpatterns = [
     path("", views.index, name="index"),
     path("golf-courses/", views.golf_courses, name="golf-courses"),
+    path("golf-courses/<int:pk>/", views.golf_course_detail, name="golf-course-detail"),
 ]
--- app/home/views.py
+++ app/home/views.py
@@ -10,3 +10,7 @@ def golf_courses(request):
     course_list = models.GolfCourse.objects.all()
     return render(request, "home/golf-courses.html", {"course_list": course_list})
 
+
+def golf_course_detail(request, pk):
+    course_data = get_object_or_404(models.GolfCourse, pk=pk)
+    return render(request, "home/golf-course-detail.html", {"obj": course_data})
