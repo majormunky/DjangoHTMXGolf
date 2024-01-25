71e07925944038c52415ea3649b7eae076b8a875
Josh Bright <josh.bright@gmail.com>
2024-01-25 10:11:18 -0800

    Added golf courses page

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from . import models
 
 
 def index(request):
     return render(request, "home/index.html", {})
+
+
+def golf_courses(request):
+    course_list = models.GolfCourse.objects.all()
+    return render(request, "home/golf-courses.html", {"course_list": course_list})
+
