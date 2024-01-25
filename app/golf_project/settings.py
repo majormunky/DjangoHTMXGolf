ec0349274e81efeafc54083435c6ded6ccdb3d9f
Josh Bright <josh.bright@gmail.com>
2024-01-25 10:03:29 -0800

    Setup core user as default user model, added Golf Course model

--- app/golf_project/settings.py
+++ app/golf_project/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "home"
+    "home",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Custom user model
+AUTH_USER_MODEL = "core.CoreUser"
--- app/home/models.py
+++ app/home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+HOLE_CHOICES = (
+    ("9", "9 Holes"),
+    ("18", "18 Holes"),
+)
+
+
+class GolfCourse(models.Model):
+    name = models.CharField(max_length=128)
+    hole_count = models.CharField(
+        max_length=64, choices=HOLE_CHOICES, default=HOLE_CHOICES[0][0]
+    )
+    tee_time_link = models.URLField(blank=True)
+    website_link = models.URLField(blank=True)
+    city = models.CharField(max_length=128, blank=True)
+    state = models.CharField(max_length=128, blank=True)
+    zip_code = models.CharField(max_length=128, blank=True)
+
+    def __str__(self):
+        return self.name
