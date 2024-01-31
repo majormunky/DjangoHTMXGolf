4847b20aaa484c49173e7870779dd4dc628a8136
Josh Bright <josh.bright@gmail.com>
2024-01-31 14:08:15 -0800

    Added model method to get holes played field in human format

--- home/models.py
+++ home/models.py
@@ -61,6 +61,11 @@ class Game(models.Model):
         self.date_played = timezone.now()
         self.save()
 
+    def get_holes_played(self):
+        if self.holes_played in ["9", "18"]:
+            return self.holes_played
+        return self.holes_played.replace("-", " ").title()
+        
 
 class Player(models.Model):
     name = models.CharField(max_length=64)
