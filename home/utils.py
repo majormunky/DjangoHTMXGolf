fa24469a70ff5a073be0b79c74f2757165ced44a
Josh Bright <josh.bright@gmail.com>
2024-01-31 13:41:57 -0800

    Added util function to create holes for course, updated logic for creating hole scores when starting game

--- home/utils.py
+++ home/utils.py
@@ -15,9 +15,27 @@ def get_form_by_slug(slug):
     return form
 
 
+def create_holes_for_course(course_data):
+    hole_count = int(course_data.hole_count)
+    for hole in range(hole_count):
+        new_hole = models.Hole(name=f"Hole: {hole + 1}", order=hole, course=course_data)
+        new_hole.save()
+
+
 def setup_scores_for_game(game_data):
+    hole_configuration = game_data.holes_played
     links = models.PlayerGameLink.objects.filter(game=game_data)
-    holes = models.Hole.objects.filter(course=game_data.course).order_by("order")
+
+    if hole_configuration in ["9", "18"]:
+        holes = models.Hole.objects.filter(course=game_data.course).order_by("order")
+    elif hole_configuration == "front-9":
+        holes = models.Hole.objects.filter(
+            course=game_data.course, order__lte=8
+        ).order_by("order")
+    elif hole_configuration == "back-9":
+        holes = models.Hole.objects.filter(
+            course=game_data.course, order__gte=9
+        ).order_by("order")
 
     for hole in holes:
         for link in links:
@@ -27,9 +45,5 @@ def setup_scores_for_game(game_data):
 
             if not existing_score:
                 hole_score = models.HoleScore.objects.create(
-                    hole=hole,
-                    current_par=hole.par,
-                    score=0,
-                    game_link=link
+                    hole=hole, current_par=hole.par, score=0, game_link=link
                 )
-            
