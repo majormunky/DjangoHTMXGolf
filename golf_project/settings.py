fb30b09d4ffd714c4cc8b3e500a896a1f575ec59
Josh Bright <josh.bright@gmail.com>
2024-01-30 17:17:59 -0800

    Updated settings to use postres db when not in debug

--- golf_project/settings.py
+++ golf_project/settings.py
@@ -81,12 +81,25 @@ WSGI_APPLICATION = "golf_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
+        "HOST": os.environ.get("DATABASE_URL"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
+
 
 
 # Password validation
