03e0104a181cc3a0e1b4d0c1f8c2b76f2dfeb676
Josh Bright <josh.bright@gmail.com>
2024-02-07 20:03:39 -0800

    Setup static files

--- golf_project/settings.py
+++ golf_project/settings.py
@@ -147,7 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-if not DEBUG:
+
+if DEBUG:
+    STATICFILES_DIRS = [BASE_DIR / "static"]
+else:
     STATIC_ROOT = BASE_DIR / "static"
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
--- golf_project/urls.py
+++ golf_project/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
