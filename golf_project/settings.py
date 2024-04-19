2bcd55f8249f0eed3d54fd0599c4c614a1db2746
Josh Bright <josh.bright@gmail.com>
2024-04-19 13:59:09 -0700

    Updated settings with csrf trusted origin setting, updated allowed hosts

--- golf_project/settings.py
+++ golf_project/settings.py
@@ -29,8 +29,10 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG") == "True"
 
-ALLOWED_HOSTS = ["*", "golf.joshbright.com", "127.0.0.1"]
-
+ALLOWED_HOSTS = ["*", "golf.joshbright.dev", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://golf.joshbright.dev",
+]
 
 # Application definition
 
