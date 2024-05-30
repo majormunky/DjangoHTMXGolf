2b3faf6195da44c5cdff16bb10de91d7f160799a
Josh Bright <josh.bright@gmail.com>
2024-05-30 13:38:07 -0700

    Added nginx container

--- app/golf_project/settings.py
+++ app/golf_project/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "static"
 
 
 # if DEBUG:
