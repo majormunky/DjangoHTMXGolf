25d0e2c56fe54a4ac6f440457f9ffe8ad787d937
Josh Bright <josh.bright@gmail.com>
2024-05-31 10:40:49 -0700

    Removed gunicorn file

--- gunicorn.conf.py
+++
@@ -1,6 +0,0 @@
-bind = "unix:/run/gunicorn.sock"
-workers = 3
-accesslog = "/home/josh/logs/gunicorn.access.log"
-errorlog = "/home/josh/logs/gunicorn.error.log"
-capture_output = True
-loglevel = "error"
